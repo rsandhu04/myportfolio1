7695c14d31f0fec9fd3226b6847091da7d8c4240
rsandhu04 <rkaursandhu04@gmail.com>
2023-10-09 21:20:44 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function biggerImg(x)
+	{
+	x.style.height="90%";
+	x.style.width="80%";
+	document.getElementById("myImage").style.left = "100%";
+	document.getElementById("myImage").style.top = "100%";
+	}
+	
+function originalImg(x)
+	{
+	x.style.height="100%";
+	x.style.width="100%";
+	document.getElementById("myImage").style.left = "90%";
+	document.getElementById("myImage").style.top = "90%";
+
+	}
